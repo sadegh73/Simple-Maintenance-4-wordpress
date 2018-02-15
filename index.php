abc35a2c4b4ae94a51c959d63a051fd133cad2be
sadegh98 <khovand.sadegh@gmail.com>
2018-02-15 16:56:54 +0330

    3

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php
 
 /*
-Plugin Name: Simple-Maintenance-4-wordpress
-Plugin URI: http://wordpress.org/
+Plugin Name: Simple-Maintenance-4-wp
+Plugin URI: https://github.com/sadegh98/Simple-Maintenance-4-wordpress
 Description: Simple Maintenance Template 
-Version: 1.0.0
+Version: 1.0.1
 
 */
 
 
 defined('ABSPATH') || exit('');
 
-function is_valid_page() {
+function sm4wp_is_valid_page() {
             return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
 }
-if( !is_admin() && !is_valid_page() ){  
-                 $name = get_bloginfo('name');
-                 echo '<title>'.$name.'</title>';
-                 define('SM_URL',plugin_dir_url(__FILE__));
-                 $image = SM_URL.'1.png';
+if( !is_admin() && !sm4wp_is_valid_page() ){  
+                 $sm4wp_title = get_bloginfo('name');
+                 echo '<title>'.$sm4wp_title.'</title>';
+                 $sm4wp_URL=  plugin_dir_url(__FILE__);
+                 $image = $sm4wp_URL.'1.png';
                  echo '<center><img src="' . $image . '"></center>';
                  die();
                 } 
