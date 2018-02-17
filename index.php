8ba5c4cd78e85ba9769157b1fb92a564240b0ece
sadegh98 <khovand.sadegh@gmail.com>
2018-02-18 03:20:18 +0330

    v1.0.2

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
 Plugin Name: Simple-Maintenance-4-wp
 Plugin URI: https://github.com/sadegh73/Simple-Maintenance-4-wordpress
 Description: Simple Maintenance Template 
-Version: 1.0.1
+Version: 1.0.2
 
 */
-
 
 defined('ABSPATH') || exit('');
 
@@ -19,7 +18,7 @@ if( !is_admin() && !sm4wp_is_valid_page() ){
                  echo '<title>'.$sm4wp_title.'</title>';
                  $sm4wp_URL=  plugin_dir_url(__FILE__);
                  $image = $sm4wp_URL.'1.png';
-                 echo '<center><img src="' . $image . '"></center>';
+                 echo '<center><img src="' . $image . '" style="max-width: 100%;height: auto;"></center>';
                  die();
                 } 
 
